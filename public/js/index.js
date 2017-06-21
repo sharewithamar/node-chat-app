ddea642d636888e64fbee3281dc5b7368048259e
Amar <sharewithamar@gmail.com>
2017-06-21 21:05:28 +0530

    wiring broadcast setup

--- public/js/index.js
+++ public/js/index.js
@@ -1,17 +1,14 @@
 var socket = io();
-socket.on('connect', function () {
-    console.log("connected to server");
 
- /*   socket.emit('createMessage',{
-        from:'Amar',
-        text: 'Learning socket io'
-    })*/
+socket.on('connect', function () {
+  console.log('Connected to server');
+
 });
 
 socket.on('disconnect', function () {
-    console.log('Disconnected from server');
+  console.log('Disconnected from server');
 });
 
 socket.on('newMessage', function (message) {
-    console.log('New message',message);
+  console.log('newMessage', message);
 });
--- server/server.js
+++ server/server.js
@@ -1,42 +1,36 @@
-const express = require('express');
 const path = require('path');
 const http = require('http');
+const express = require('express');
 const socketIO = require('socket.io');
+
 const publicPath = path.join(__dirname, '../public');
 const port = process.env.PORT || 3000;
-//console.log(__dirname + '/../public');
-//console.log(publicPath);
-
-const app = express();
+var app = express();
 var server = http.createServer(app);
 var io = socketIO(server);
-io.on('connection', (socket) => {
-    console.log("new user connected");
-
-    socket.on('disconnect', () => {
-        console.log("User was disconnected");
-    });
-
-   socket.on('createMessage',(message)=>{
-    console.log('create Message',message);
-   /*  io.emit('newMessage',{
-         from: message.from,
-         text:message.text,
-         createdAt:new Date().getTime()
-     });*/
-
-   })
-
-/*    socket.emit('newMessage',{
-        from: 'Amarnath Rajasekaran',
-        text:'Andrew Mead course',
-        createdAt: new Date().getTime()
-
-    });*/
-});
 
 app.use(express.static(publicPath));
 
+io.on('connection', (socket) => {
+  console.log('New user connected');
+
+
+  socket.on('createMessage', (message) => {
+    console.log('createMessage', message);
+     io.emit('newMessage',{
+         from: message.from,
+         text: message.text,
+         createdAt: new Date().getTime()
+     });
+
+
+  });
+
+  socket.on('disconnect', () => {
+    console.log('User was disconnected');
+  });
+});
+
 server.listen(port, () => {
-    console.log(`server is up at${port}`);
-})
+  console.log(`Server is up on ${port}`);
+});
