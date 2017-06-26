68ecb266f76dbe8bf8586a54a7edda30db77b464
Amarnath Rajasekaran <sharewithamar@gmail.com>
2017-06-26 16:11:46 +0530

    moment playground

---
+++ playground/time.js
@@ -0,0 +1,12 @@
+// let currentMonth = new Date().getMonth();
+// console.log('currentMonth', currentMonth);
+
+
+var moment = require('moment');
+
+var date = moment();
+//date.add(1, 'Y').subtract(12, 'months');
+//console.log(date.format('MMM Do YYYY'));
+
+//console.log(date.calendar());
+console.log(date.format('h:ss a'));
